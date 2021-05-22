f8884067bd21464e757f226941df058aac68d703
Meredith Nachman <mwnachman@gmail.com>
2021-05-21 23:03:26 -0400

    add x or y

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -1,5 +1,12 @@
 import React, {useState} from 'react'
-import {Card, Grid} from '@material-ui/core'
+import {Card,
+        FormControl,
+        FormControlLabel,
+        FormLabel,
+        Grid,
+        Radio,
+        RadioGroup,
+        Typeography} from '@material-ui/core'
 
 import Board from './Board.jsx'
 import useStyles from '../style/game'
@@ -7,10 +14,12 @@ import { checkForWin, findMove } from '../helpers/helpers';
 
 const Game = () => {
   const [board, updateBoard] = useState(new Array(9).map(() => null))
+  const [xOrY, updateXorY] = useState(undefined)
+
 
   const makeMove = () => {
     console.log('booyah')
-  //   let boardCopy = game.slice();
+  //   let boardCopy = board.slice();
   //   boardCopy[square] = player.xOrY;
   //   const computerPlayer = player.xOrY === "X" ? "O" : "X";
   //   if (player.playersTurn === true && game[square] === null) {
@@ -31,6 +40,16 @@ const Game = () => {
           className={styles.exterior_grid}>
       <Grid item xs={8} className={styles.interior_grid}>
         <Card className={styles.root}>
+          <div  className={styles.xOrY}>
+
+          <FormControl component="fieldset">
+            <FormLabel className={styles.formLabel}>Would you like to be X or O?</FormLabel>
+            <RadioGroup row name="role">
+              <FormControlLabel value="X" control={<Radio />} label="X" />
+              <FormControlLabel value="O" control={<Radio />} label="O" />
+            </RadioGroup>
+          </FormControl>
+          </div>
           <Board makeMove={makeMove} board={board}/>
         </Card>
       </Grid>
--- client/src/style/game.js
+++ client/src/style/game.js
@@ -6,7 +6,7 @@ const useStyles = makeStyles(theme => ({
     marginTop: theme.spacing(2),
     marginBottom: theme.spacing(6),
     width: '100%',
-    height: '600px'
+    height: '600px',
   },
   exterior_grid: {
     minHeight: '70vh',
@@ -14,6 +14,13 @@ const useStyles = makeStyles(theme => ({
   interior_grid: {
     minWidth: '90%',
   },
+  xOrY: {
+    width: 320,
+    margin: '0 auto'
+  },
+  formLabel: {
+    fontSize: 24,
+  }
 }))
 
 export default useStyles
